1f428cd01e613f9444e885bc35cbd851641cee04
juicewa <palayjoshua0@gmail.com>
2026-02-20 15:49:47 +0800

    new apitble account, created new github registry

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+echo "Testing DB connection..."
+mysql -h "$DB_HOST" -P "$DB_PORT" -u "$DB_USERNAME" -p"$DB_PASSWORD" -e "SELECT 1;" || exit 1
+
 echo "Starting Laravel container initialization..."
 
 php artisan config:clear
